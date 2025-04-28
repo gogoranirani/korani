4e77700863061bca31061ed7faba809480f3139b
gogoranirani <bjhl85@gmail.com>
2025-04-29 00:27:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-from flask import Flask, render_template, request, session
-import random
-import os
-
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     result = None
@@ -23,6 +16,10 @@ def index():
 
         plus_randoms = [estimate_price * random.uniform(0, 2) for _ in range(8)]
         minus_randoms = [estimate_price * random.uniform(-2, 0) for _ in range(7)]
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))  # Render가 주는 포트를 사용
-    app.run(host='0.0.0.0', port=port)
+        all_randoms = plus_randoms + minus_randoms
+        selected = random.sample(all_randoms, 4)
+        planned_price = sum(selected) / 4
+        final_price = ((planned_price - a_value) * 0.87745) + a_value
+        result = round(final_price, 2)
+
+    return render_template('index.html', result=result, estimate_price=session.get('estimate_price'), a_value=session.get('a_value'))
